6228918395d15ffef11788ade037c3619344a12a
Corinne Pingul <corinnepingul@gmail.com>
2015-10-13 12:54:54 -0700

    Adds seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+CSV.foreach("db/users.csv", headers: true, header_converters: :symbol, converters: :all) do |row|
+  User.create(
+    username: row[:username],
+    email: row[:email],
+    password_digest: row[:password_digest],
+    phone_number: row[:phone_number],
+    locale: row[:locale]
+  )
+end
+
+CSV.foreach("db/moments.csv", headers: true, header_converters: :symbol, converters: :all) do |row|
+  Moment.create(
+    date: row[:date],
+    body: row[:body],
+    user_id: row[:user_id]
+  )
+end
+
+CSV.foreach("db/tags.csv", headers: true, header_converters: :symbol, converters: :all) do |row|
+  Tag.create(
+    body: row[:body]
+  )
+end
