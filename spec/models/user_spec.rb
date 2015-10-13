a446769624fe68db2d063064c1cf1823ab9c07b0
Corinne Pingul <corinnepingul@gmail.com>
2015-10-13 13:40:32 -0700

    DRYing up some code.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,22 +14,24 @@ RSpec.describe User, type: :model do
   describe "user model validations" do
     let(:user) { create(:user) }
 
+    it "is valid" do
+      expect(user).to be_valid
+    end
+
     it "requires a unique username" do
-      user # valid user
+      user
       user_without_username = build(:user, username: nil) # invalid user (no name)
       user_without_unique_username = build(:user) # invalid user (non-unique name)
 
-      expect(user).to be_valid
       expect(user_without_username).to_not be_valid
       expect(user_without_unique_username).to_not be_valid
     end
 
     it "requires a unique email" do
-      user # valid user
+      user
       user_without_email = build(:user, email: nil) # invalid user (no email)
       user_without_unique_email = build(:user) # invalid user (non-unique email)
 
-      expect(user).to be_valid
       expect(user_without_email).to_not be_valid
       expect(user_without_unique_email).to_not be_valid
     end
@@ -45,7 +47,6 @@ RSpec.describe User, type: :model do
       user_without_phone_number = build(:user, phone_number: nil) # invalid user (no phone_number)
       user_without_unique_phone_number = build(:user) # invalid user (non-unique phone_number)
 
-      expect(user).to be_valid
       expect(user_without_phone_number).to_not be_valid
       expect(user_without_unique_phone_number).to_not be_valid
     end
