efe3a4e2e616fa94ac21ab083bee454113396194
Corinne Pingul <corinnepingul@gmail.com>
2015-10-14 15:09:53 -0700

    Adds redirect to login page when user is not logged in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_filter :require_login, only: [:index, :create]
+
+  def index; end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "GET #index" do
+    it "does not require login" do
+      session[:id] = nil
+      get :index
 
+      expect(flash[:error]).to be_nil
+      expect(response).not_to redirect_to(login_path)
+    end
+
+    # it "renders the index template" do
+    #   get :index
+    #
+    #   expect(response).to render_template("index")
+    # end
+  end
 end
