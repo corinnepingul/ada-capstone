878e7536bda92e7e7ddced2f4363e0c7064aa479
Corinne Pingul <corinnepingul@gmail.com>
2015-10-15 15:36:52 -0700

    Adds secure password to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   # Associations ---------------------------------------------------------------
   has_and_belongs_to_many :tags
   has_many :moments
