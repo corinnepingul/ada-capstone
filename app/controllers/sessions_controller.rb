e37ecf10977ec423298a51400f36af0ace3b33d7
Corinne Pingul <corinnepingul@gmail.com>
2015-10-15 15:38:51 -0700

    Adds session#index and session#create methods and tests.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,25 @@
 class SessionsController < ApplicationController
   skip_before_filter :require_login, only: [:index, :create]
+  before_action :set_user, only: [:create]
 
-  def index; end
+  def index
+    @user = User.new
+  end
+
+  def create # STOPPED HERE!
+    if @user && @user.authenticate(params[:session][:password])
+      session[:id] = @user.id
+      flash[:messages] = MESSAGES[:successful_login]
+      redirect_to root_path(@user)
+    else
+      flash.now[:errors] = ERRORS[:login_error] # NOTE: in specs, can test this with: `@sellers.errors.messages`
+      render :index
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(username: params[:session][:username])
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,13 +7,42 @@ RSpec.describe SessionsController, type: :controller do
       get :index
 
       expect(flash[:error]).to be_nil
-      expect(response).not_to redirect_to(login_path)
+      expect(response).not_to redirect_to(registration_path)
     end
 
-    # it "renders the index template" do
-    #   get :index
-    #
-    #   expect(response).to render_template("index")
-    # end
+    it "renders the index template" do
+      get :index
+
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "POST #create" do
+    context "valid user params" do
+      let(:user_login_params) { { :session => attributes_for(:user) } }
+
+      before :each do
+        @user = create(:user)
+
+        post :create, user_login_params
+      end
+
+      it "sets the user" do
+        expect(assigns(:user)).to eq @user
+      end
+
+      it "sets session[:id] to the user's id" do
+        expect(session[:id]).to eq @user.id
+      end
+
+      it "updates the flash[:messages] to include :successful_login" do
+        expect(flash[:messages]).to include { :successful_login }
+      end
+
+      it "redirects to the user's homepage" do
+        expect(response).to redirect_to root_path(@user.id)
+        expect(response).to have_http_status(302)
+      end
+    end
   end
 end
