4e216edbed82b1610e2d8f2b0c1087a5dffaa32c
Corinne Pingul <corinnepingul@gmail.com>
2015-10-19 19:21:50 -0700

    Directs user to correct path depeneding on if the user is verified or not.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :require_login, only: [:new, :create]
+  skip_before_filter :require_verfied_user, only: [:new, :create]
   before_action :set_user, only: [:create]
 
   def new
@@ -9,8 +10,15 @@ class SessionsController < ApplicationController
   def create
     if @user && @user.authenticate(params[:session][:password])
       session[:id] = @user.id
-      flash[:messages] = MESSAGES[:successful_login]
-      redirect_to root_path(@user)
+
+      if @user.verified == true
+        flash[:messages] = MESSAGES[:successful_login]
+        redirect_to root_path(@user)
+      else
+        flash[:notice] = { notice: "Please verify your account. Verification code resent." }
+        redirect_to verify_path
+      end
+
     else
       flash[:errors] = ERRORS[:login_error]
       redirect_to registration_path
