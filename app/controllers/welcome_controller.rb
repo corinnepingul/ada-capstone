ce345a270be2ade59e3b88a4abb677288217dbe6
Corinne Pingul <corinnepingul@gmail.com>
2015-10-19 19:23:06 -0700

    Removes redirect from index method.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   before_action :set_user, only: [:index]
 
   def index
-    redirect_to verify_path if @user.verified == false
     @moments = @user.moments
   end
 
