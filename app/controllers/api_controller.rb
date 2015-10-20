f6b8fb3c2ba403088b5790ab8389ee82bdf7deb2
Corinne Pingul <corinnepingul@gmail.com>
2015-10-20 14:47:04 -0700

    Adds some potential code to the api controller.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,6 +3,19 @@ class ApiController < ApplicationController
   skip_before_filter :require_verfied_user
 
   def received_message
+    message_body = params["Body"]
+    from_number = params["From"]
 
+    # finds the user who's sending in the text (may need to parse this data)
+    user = User.where(phone_number: from_number)
+
+    # if the user is found, create a new moment for them
+    unless user.nil?
+      @moment = Moment.create(
+                  date: Date.new,
+                  body: message_body,
+                  user_id: user.id
+                  )
+    end
   end
 end
