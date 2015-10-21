949a5d0e8a8d1fc5745a9079f8483c002b1bfade
Corinne Pingul <corinnepingul@gmail.com>
2015-10-21 16:18:34 -0700

    Testing something in the api#received_message controller action.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,24 +4,32 @@ class ApiController < ApplicationController
   skip_before_filter :set_locale
   skip_before_action :verify_authenticity_token
 
-  def received_message
-    puts "hello"
-    puts params
+  # TWILIO's POST REQUEST:
+  # params => {"ToCountry"=>"US", "ToState"=>"LA", "SmsMessageSid"=>"SM67b107936f3a02e9c75c4f9b048ede91",
+  # "NumMedia"=>"0", "ToCity"=>"NEW ORLEANS", "FromZip"=>"98104", "SmsSid"=>"SM67b107936f3a02e9c75c4f9b048ede91",
+  # "FromState"=>"WA", "SmsStatus"=>"received", "FromCity"=>"SEATTLE", "Body"=>"Yo", "FromCountry"=>"US",
+  # "To"=>"+15043157972", "ToZip"=>"70149", "NumSegments"=>"1", "MessageSid"=>"SM67b107936f3a02e9c75c4f9b048ede91",
+  # "AccountSid"=>"AC6298a5335759e854332607f3a69bf92e", "From"=>"+12064997650", "ApiVersion"=>"2010-04-01",
+  # "controller"=>"api", "action"=>"received_message"}
 
-    head 200, content_type: "text/html"
-    # message_body = params["Body"]
-    # from_number = params["From"]
-    #
-    # # finds the user who's sending in the text (may need to parse this data)
-    # user = User.where(phone_number: from_number)
-    #
-    # # if the user is found, create a new moment for them
-    # unless user.nil?
-    #   @moment = Moment.create(
-    #               date: Date.new,
-    #               body: message_body,
-    #               user_id: user.id
-    #               )
-    # end
+  def received_message
+    message_body = params["Body"]
+    from_number = params["From"]
+
+    # finds the user who's sending in the text (may need to parse this data)
+    user = User.where(phone_number: from_number)
+
+    # if the user is found, create a new moment for them
+    unless user.nil?
+      @moment = Moment.new(
+                  date: Date.new,
+                  body: message_body,
+                  user_id: user.id
+                  )
+
+      if @moment.save
+        puts "This moment was saved!"
+      end
+    end
   end
 end
