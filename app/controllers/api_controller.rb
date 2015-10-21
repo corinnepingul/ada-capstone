5a2fd1126c74187b5a36d577d1533a4209212421
Corinne Pingul <corinnepingul@gmail.com>
2015-10-21 13:00:16 -0700

    Skips before action, set_locale for api controller actions.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,7 @@
 class ApiController < ApplicationController
   skip_before_filter :require_login
   skip_before_filter :require_verfied_user
+  skip_before_filter :set_locale
 
   def received_message
     puts "hello"
