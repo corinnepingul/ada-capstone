b480574863b92b4a548c08ddea0d7a30dc728ec0
Corinne Pingul <corinnepingul@gmail.com>
2015-10-22 11:47:36 -0700

    Refactors a lot of users controller to not use instance variables and puts send verification code in the create method instead of the verify method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,17 @@ class UsersController < ApplicationController
   skip_before_filter :require_verfied_user, only: [:create, :show_verify, :verify, :resend]
 
   def create
-    @valid_user_params = valid_user_params
-    add_country_code_to_phone_number(@valid_user_params)
+    user_attributes = valid_user_params
 
-    @user = User.new(@valid_user_params)
+    # this changes phone_number field to include country code before saving record
+    add_country_code_to_phone_number!(user_attributes)
 
-    if @user.save
-      session[:id] = @user.id
-      register_authy_user(@user)
+    user = User.new(user_attributes)
+
+    if user.save
+      session[:id] = user.id
+      register_authy_user(user)
+      send_verification_code_to_current_user
 
       redirect_to verify_path
     else
@@ -25,23 +28,17 @@ class UsersController < ApplicationController
   def show_verify
     # if there's no user logged in, redirect to registration path
     return redirect_to registration_path unless session[:id]
-
-    # otherwise, send them a code
-    @user = current_user
-
-    # Send an SMS with verifcation code to user, see authy_client.rb
-    AuthyClient.send_verification_code(@user.authy_id)
   end
 
   def verify
-    @user = current_user
+    user = current_user
 
     # Use Authy to send the verification token to API
-    token = AuthyClient.verify_token(@user.authy_id, params[:token])
+    token = AuthyClient.verify_token(user.authy_id, params[:token])
 
     if token.ok?
       # Mark the user as verified
-      @user.update(verified: true)
+      user.update(verified: true)
       flash[:message] = MESSAGES[:successful_login]
 
       redirect_to root_path
@@ -53,15 +50,20 @@ class UsersController < ApplicationController
   end
 
   def resend
-    @user = current_user
-    AuthyClient.send_verification_code(@user.authy_id)
-
+    send_verification_code_to_current_user
     flash[:notices] = NOTICES[:verification_code_resent]
 
     redirect_to verify_path
   end
 
   private
+
+  def send_verification_code_to_current_user
+    user = current_user
+
+    # Send an SMS with verifcation code to user, see authy_client.rb
+    AuthyClient.send_verification_code(user.authy_id)
+  end
 
   def register_authy_user(user)
     # Registers the user in Authy (to send them a text), see authy_client.rb
@@ -72,10 +74,18 @@ class UsersController < ApplicationController
   end
 
   def valid_user_params
-    params.require(:user).permit(:username, :email, :phone_number, :country_code, :password, :password_confirmation, :locale)
+    params.require(:user).permit(
+                            :username,
+                            :email,
+                            :phone_number,
+                            :country_code,
+                            :password,
+                            :password_confirmation,
+                            :locale
+                            )
   end
 
-  def add_country_code_to_phone_number(valid_user_params)
-    valid_user_params[:phone_number] = "+" + valid_user_params[:country_code] + valid_user_params[:phone_number]
+  def add_country_code_to_phone_number!(user_attributes)
+    user_attributes[:phone_number] = "+" + user_attributes[:country_code] + user_attributes[:phone_number]
   end
 end
