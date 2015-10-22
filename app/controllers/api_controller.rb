5456fb8f1089eb0a8e82d1b44eb8c11c8db842d5
Corinne Pingul <corinnepingul@gmail.com>
2015-10-22 14:31:15 -0700

    Removes puts.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,7 @@ class ApiController < ApplicationController
   skip_before_filter :set_locale
   skip_before_action :verify_authenticity_token
 
-  # TWILIO's POST REQUEST:
+  # TWILIO's POST REQUEST FOR SMS:
   # params => {"ToCountry"=>"US", "ToState"=>"LA", "SmsMessageSid"=>"SM67b107936f3a02e9c75c4f9b048ede91",
   # "NumMedia"=>"0", "ToCity"=>"NEW ORLEANS", "FromZip"=>"98104", "SmsSid"=>"SM67b107936f3a02e9c75c4f9b048ede91",
   # "FromState"=>"WA", "SmsStatus"=>"received", "FromCity"=>"SEATTLE", "Body"=>"Yo", "FromCountry"=>"US",
@@ -12,8 +12,17 @@ class ApiController < ApplicationController
   # "AccountSid"=>"AC6298a5335759e854332607f3a69bf92e", "From"=>"+12064997650", "ApiVersion"=>"2010-04-01",
   # "controller"=>"api", "action"=>"received_message"}
 
+  # TWILIO's POST REQUEST FOR MMS:
+  # {"ToCountry"=>"US", "MediaContentType0"=>"image/jpeg", "ToState"=>"LA",
+  # "SmsMessageSid"=>"MMa39e75e57c740de6c997237fc13a0582", "NumMedia"=>"1",
+  # "ToCity"=>"NEW ORLEANS", "FromZip"=>"98104", "SmsSid"=>"MMa39e75e57c740de6c997237fc13a0582",
+  # "FromState"=>"WA", "SmsStatus"=>"received", "FromCity"=>"SEATTLE", "Body"=>"", "FromCountry"=>"US",
+  # "To"=>"+15043157972", "ToZip"=>"70149", "NumSegments"=>"1", "MessageSid"=>"MMa39e75e57c740de6c997237fc13a0582",
+  # "AccountSid"=>"AC6298a5335759e854332607f3a69bf92e", "From"=>"+12064997650",
+  # "MediaUrl0"=>"https://api.twilio.com/2010-04-01/Accounts/AC6298a5335759e854332607f3a69bf92e/Messages/MMa39e75e57c740de6c997237fc13a0582/Media/ME3952542eb62296b2fbefb42bd894b3f5",
+  # "ApiVersion"=>"2010-04-01", "controller"=>"api", "action"=>"received_message"}
+
   def received_message
-    puts params
     message_body = params["Body"]
     from_number = params["From"]
 
