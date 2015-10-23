a49d2f9be258a7b50d4fa13e7333f61fecb6786a
Corinne Pingul <corinnepingul@gmail.com>
2015-10-23 13:07:08 -0700

    Adds regex to validate phone numbers that have valid us area codes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  # accepts numbers in format of: 111-111-1111, 111.111.1111,
+  VALID_PHONE_REGEX = /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
+
   has_secure_password
   # Associations ---------------------------------------------------------------
   has_and_belongs_to_many :tags
@@ -8,5 +11,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
-  validates :phone_number, presence: true, uniqueness: true, length: { minimum: 10, maximum: 15 }
+  validates :phone_number, presence: true, uniqueness: true, length: { minimum: 10, maximum: 15 }, format: { with: VALID_PHONE_REGEX }
 end
