600853bd9e330d154f3599a1406cd6b3591e605d
Corinne Pingul <corinnepingul@gmail.com>
2015-10-27 12:50:35 -0700

    Modifies login and signup buttons.

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('.form-row').on("shown.bs.collapse", function() {
-    console.log("im here");
     $("body, html").animate({
             scrollTop: $(this).offset().top
         });
