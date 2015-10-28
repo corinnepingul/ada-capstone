32ac63e0083bb2b127cacf0f914113afecfd2d2c
Corinne Pingul <corinnepingul@gmail.com>
2015-10-27 18:20:49 -0700

    Makes alerts go away after three seconds.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$('document').ready(function() {
+  setTimeout(function() {
+    $('.alert').slideUp();
+  }, 3000);
+});
