0bd2af2b0f85fb3ea1d05e6943a8088d254e723a
Corinne Pingul <corinnepingul@gmail.com>
2015-10-27 18:06:51 -0700

    Deletes js file for sessions because not using collapse.

--- app/assets/javascripts/sessions.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.form-row').on("shown.bs.collapse", function() {
-    $("body, html").animate({
-            scrollTop: $(this).offset().top
-        });
-  });
-});
