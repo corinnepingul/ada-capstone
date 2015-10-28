2cfafbb9ee232bf6d973790c24b5245d3ff78add
Corinne Pingul <corinnepingul@gmail.com>
2015-10-28 09:42:42 -0700

    Adds background photo for registration page and verification page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def flash_message_margin(session_id)
     session_id ? "flash_message_margin" : ""
   end
+
+  def background_photo(session_id)
+    session_id ? "boat" : "nye"
+  end
 end
