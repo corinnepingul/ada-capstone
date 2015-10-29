c24e7afd5a6cb2905d04db90af3cdc0028e424a4
Corinne Pingul <corinnepingul@gmail.com>
2015-10-29 07:50:05 -0700

    Single quote to double qoute.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "/registration", to: "sessions#new", as: "registration" # login/signup page
   post "/login", to: "sessions#create", as: "login"           # creates session -> homepage
   delete "/logout", to: "sessions#destroy", as: "logout"      # deletes session -> login/signup page
-  get "users/verify", to: 'users#show_verify', as: 'verify'   # verification code form page
+  get "users/verify", to: 'users#show_verify', as: "verify"   # verification code form page
   post "users/verify"                                       # creates user -> homepage
   post "users/resend"                                         # resends code -> verification code form page
 
