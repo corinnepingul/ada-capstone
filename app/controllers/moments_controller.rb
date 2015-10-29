736569a00e61fa141dfa39fe5e2073ed82d41ca3
Corinne Pingul <corinnepingul@gmail.com>
2015-10-29 08:43:42 -0700

    WIP moments#search.

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -1,2 +1,11 @@
 class MomentsController < ApplicationController
+
+  def search
+    raise # what are my params here
+
+    # build psql statement here
+    # select * from moments where user_id = 2 and body like '%things%' and body like '%friend%';
+    # user_moments = User.find(session[:id]).moment
+  end
+
 end
