915f1e5f60aad3ec5fadc0b39f76d80bd79f5dc6
Corinne Pingul <corinnepingul@gmail.com>
2015-10-29 20:46:07 -0700

    Adds if statement to index action for ajax call for year.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,20 +2,17 @@ class WelcomeController < ApplicationController
   before_action :set_user, only: [:index]
 
   def index
-    # only loads moments from the current year
-    @moments = Moment.user(session[:id]).current_year.reverse
-
     @homepage = "homepage"
-    @years = users_years(@user)
-  end
+    @years_array = users_years(@user)
+    @year = params[:year] || Date.today.year.to_s
 
-  def year
-    @moments = Moment.user(session[:id]).year(params[:year]).reverse
-
-    @homepage = @moments.empty? ? "empty-search" : "homepage"
-    @years = users_years(@user)
-
-    render :index
+    if params[:year] # loads moments from selected year
+      @moments = Moment.user(session[:id]).year(params[:year]).reverse
+      render partial: "welcome/timeline"
+    else # default only loads moments from the current year
+      @moments = Moment.user(session[:id]).current_year.reverse
+      render :index
+    end
   end
 
   private
@@ -25,16 +22,16 @@ class WelcomeController < ApplicationController
   end
 
   def users_years(user)
-    # signup_year = user.created_at.year
-    signup_year = 2001
+    signup_year = user.created_at.year
+    # signup_year = 2001
     current_year = Date.today.year
 
     year_array = []
 
-    year_counter = signup_year
-    while year_counter <= current_year
+    year_counter = current_year
+    while year_counter >= signup_year
       year_array.push(year_counter.to_s)
-      year_counter += 1
+      year_counter -= 1
     end
 
     return year_array
